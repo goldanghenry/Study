09bed566bdcb93409ed48c1578dccbfe5d4b98f2
goldanghenry <wshdhkd@naver.com>
2022-12-21 18:47:16 +0900

    chapter2

---
+++ Java/chapter2/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+    public static int sum(int n, int m){
+        return n+m;
+    }
+    public static void main(String[] args){
+        int i = 20;
+        int s;
+        
+        s = sum(i,10);
+        System.out.println(s);
+    }
+}
