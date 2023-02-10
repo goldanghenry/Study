c9e9a7ef08ee030e80316832b5ea26c4ea084a74
우성현 <wshdhkd@naver.com>
2023-02-10 21:31:41 +0900

    spring-boot jpa Entity, Repository

---
+++ SpringBoot/jpa/jpa/src/main/java/com/springboot/jpa/data/dao/ProductDAO.java
@@ -0,0 +1,8 @@
+package com.springboot.jpa.data.dao;
+
+import com.springboot.jpa.data.entity.Product;
+
+public interface ProductDAO {
+    Product insertProduct(Product product);
+
+}
---
+++ SpringBoot/jpa/jpa/src/main/java/com/springboot/jpa/data/dao/impl/ProductDAOImpl.java
@@ -0,0 +1,4 @@
+package com.springboot.jpa.data.dao.impl;
+
+public class ProductDAOImpl {
+}
