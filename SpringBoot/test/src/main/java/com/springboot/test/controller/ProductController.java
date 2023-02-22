9364af8ce7c760bc1892569af1a6f2a50833be27
우성현 <wshdhkd@naver.com>
2023-02-22 18:12:26 +0900

    Advanced - Spring Data JPA

---
+++ SpringBoot/test/src/main/java/com/springboot/test/controller/ProductController.java
@@ -0,0 +1,47 @@
+package com.springboot.test.controller;
+
+import com.springboot.test.data.dto.ChangeProductNameDto;
+import com.springboot.test.data.dto.ProductDto;
+import com.springboot.test.data.dto.ProductResponseDto;
+import com.springboot.test.service.ProductService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+    private final ProductService productService;
+    @Autowired
+    public ProductController(ProductService productService){
+        this.productService = productService;
+    }
+    @ApiOperation(value = "상품 조회")
+    @GetMapping()
+    public ResponseEntity<ProductResponseDto> getProduct(Long number){
+        ProductResponseDto productResponseDto = productService.getProduct(number);
+        return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
+    }
+    @ApiOperation(value = "상품 추가")
+    @PostMapping()
+    public ResponseEntity<ProductResponseDto> createProduct(@RequestBody ProductDto productDto){
+        ProductResponseDto productResponseDto = productService.saveProduct(productDto);
+        return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
+    }
+    @ApiOperation(value = "상품 이름 변경")
+    @PutMapping()
+    public ResponseEntity<ProductResponseDto> changeProductName(@RequestBody ChangeProductNameDto changeProductNameDto) throws Exception {
+        ProductResponseDto productResponseDto = productService.changeProductName(
+                changeProductNameDto.getNumber(),
+                changeProductNameDto.getName());
+        return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
+    }
+    @ApiOperation(value = "상품 삭제")
+    @DeleteMapping()
+    public ResponseEntity<String> deleteProduct(Long number) throws Exception{
+        productService.deleteProduct(number);
+        return ResponseEntity.status(HttpStatus.OK).body("정상적으로 삭제되었습니다.");
+    }
+}
