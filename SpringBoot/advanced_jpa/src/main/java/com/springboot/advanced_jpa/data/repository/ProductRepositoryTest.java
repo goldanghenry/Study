4e9745695e26759297063d2d22adf8bc619bcd7e
우성현 <wshdhkd@naver.com>
2023-02-23 15:28:10 +0900

    정렬 - Spring Data JPA

---
+++ SpringBoot/advanced_jpa/src/main/java/com/springboot/advanced_jpa/data/repository/ProductRepositoryTest.java
@@ -0,0 +1,57 @@
+package com.springboot.advanced_jpa.data.repository;
+
+import com.springboot.advanced_jpa.data.entity.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+public class ProductRepositoryTest {
+    @Autowired
+    ProductRepository productRepository;
+
+    @Test
+    void sortingAndPagingTest() {
+        Product product1 = new Product();
+        product1.setName("펜");
+        product1.setPrice(1000);
+        product1.setStock(100);
+        product1.setCreatedAt(LocalDateTime.now());
+        product1.setUpdatedAt(LocalDateTime.now());
+
+        Product product2 = new Product();
+        product1.setName("펜");
+        product1.setPrice(5000);
+        product1.setStock(300);
+        product1.setCreatedAt(LocalDateTime.now());
+        product1.setUpdatedAt(LocalDateTime.now());
+
+        Product product3 = new Product();
+        product1.setName("펜");
+        product1.setPrice(500);
+        product1.setStock(50);
+        product1.setCreatedAt(LocalDateTime.now());
+        product1.setUpdatedAt(LocalDateTime.now());
+
+        Product savedProduct1 = productRepository.save(product1);
+        Product savedProduct2 = productRepository.save(product2);
+        Product savedProduct3 = productRepository.save(product3);
+
+        productRepository.findByName("펜", Sort.by(Sort.Order.asc("price")));
+        productRepository.findByName("펜", Sort.by(Sort.Order.asc("price"), Sort.Order.desc("stock")));
+
+        System.out.println(productRepository.findByName("펜", getSort()));
+
+    }
+    // 쿼리 메서드에서 정렬 부분을 분리
+    private Sort getSort() {
+        return Sort.by(
+                Sort.Order.asc("price"),
+                Sort.Order.desc("stock")
+        );
+    }
+}
+
