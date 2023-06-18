364d08afed4640dfdcbab7757c16c1ebfeba9b25
우성현 <wshdhkd@naver.com>
2023-06-18 18:42:14 +0900

    Excel - POI API 2

---
+++ SpringBoot/MiniProject/src/main/java/kr/excel/example/entity/MemberVo.java
@@ -0,0 +1,67 @@
+package kr.excel.example.entity;
+
+public class MemberVo {
+    private String name;
+    private int age;
+    private String birthdate;
+    private String phone;
+    private String address;
+    private boolean isMarried;
+
+    public MemberVo(String name, int age, String birthdate, String phone, String address, boolean isMarried) {
+        this.name = name;
+        this.age = age;
+        this.birthdate = birthdate;
+        this.phone = phone;
+        this.address = address;
+        this.isMarried = isMarried;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public boolean isMarried() {
+        return isMarried;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setMarried(boolean married) {
+        isMarried = married;
+    }
+}
